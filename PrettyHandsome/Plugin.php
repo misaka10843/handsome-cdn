402c351e56ae37d70549e16f5c8e04885274becc
misaka10843 <misaka10843@outlook.jp>
2023-02-11 00:41:39 +0800

    update

---
+++ PrettyHandsome/Plugin.php
@@ -0,0 +1,240 @@
+<?php
+
+use Typecho\Plugin\PluginInterface;
+use Typecho\Widget\Helper\Form;
+
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
+include 'function/form.php';
+include 'function/header.php';
+include 'function/footer.php';
+
+/**
+ * <strong style="color:#28B7FF;font-family: 楷体;">Handsome主题美化专用</strong>
+ *<div class="prettyHandsome"><a style="width:fit-content" id="prettyHandsome">版本检测中..</div>&nbsp;</div><style>.prettyHandsome {    margin-top: 5px;}.prettyHandsome a {    background: #00BFFF;    padding: 5px;    color: #fff;}</style>
+ * <script>var prettyHandsome="1.3.1";function update_detec(){var container=document.getElementById("prettyHandsome");if(!container){return}var ajax=new XMLHttpRequest();container.style.display="block";ajax.open("get","https://api.github.com/repos/isSuperman/Pretty_for_handsome/releases/latest");ajax.send();ajax.onreadystatechange=function(){if(ajax.readyState===4&&ajax.status===200){var obj=JSON.parse(ajax.responseText);var newest=obj.tag_name;if(newest>prettyHandsome){container.innerHTML="发现新主题版本："+obj.name+'。下载地址：<a href="'+obj.zipball_url+'">点击下载</a>'+"<br>当前版本:"+String(prettyHandsome)+'<a target="_blank" href="'+obj.html_url+'">👉查看新版亮点</a>'}else{container.innerHTML="当前版本:"+String(prettyHandsome)+"。"+"最新版"}}}};update_detec();</script>		
+ * @package PrettyHandsome
+ * @author <strong style="color:#28B7FF;font-family: 楷体;">isSuperman</strong>
+ * @version 1.3.1
+ * @link https://github.com/isSuperman/Pretty_for_handsome
+ */
+class PrettyHandsome_Plugin implements PluginInterface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     */
+    public static function activate()
+    {
+		Typecho_Plugin::factory('admin/common.php')->begin = [__Class__, 'parseShortCode'];
+		Typecho_Plugin::factory('Widget_Archive')->handleInit = [__Class__, 'parseShortCode'];
+        Typecho_Plugin::factory('Widget_Archive')->header = array(__CLASS__, 'header');
+        Typecho_Plugin::factory('Widget_Archive')->footer = array(__CLASS__, 'footer');
+        Typecho_Plugin::factory('admin/write-post.php')->bottom = array(__CLASS__, 'insertHeader');
+        Typecho_Plugin::factory('admin/write-page.php')->bottom = array(__CLASS__, 'insertHeader');
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     */
+    public static function deactivate()
+    {
+    }
+
+    /**
+     * 获取插件配置面板
+     *
+     * @param Form $form 配置面板
+     */
+    public static function config(Form $form)
+    {
+        // 抖音解析API
+        $form->addInput(PluginsForm::DouyinApi());
+
+        // 其他视频解析API
+        $form->addInput(PluginsForm::VideoApi());
+
+        // 响应耗时和访客总数
+        $form->addInput(PluginsForm::SiteInfo());
+
+        // 时光流逝
+        $form->addInput(PluginsForm::TimeInfo());
+
+        // 顶部导航栏天气
+        $form->addInput(PluginsForm::Weather());
+        $form->addInput(PluginsForm::WeatherUID());
+        $form->addInput(PluginsForm::WeatherHASH());
+
+        // 彩色目录图标
+        $form->addInput(PluginsForm::ColorToc());
+
+        // 彩色标签云
+        $form->addInput(PluginsForm::ColorTag());
+
+        // 鼠标经过头像旋转放大
+        $form->addInput(PluginsForm::AvatarCircle());
+
+        // 鼠标点击特效
+        $form->addInput(PluginsForm::ClickWord());
+
+        // 文章标题居中
+        $form->addInput(PluginsForm::TitleCenter());
+
+        // LOGO扫光
+        $form->addInput(PluginsForm::LogoScan());
+
+        // 复制成功提示
+        $form->addInput(PluginsForm::CopyTip());
+
+        // H1/H2标题背景颜色
+        $form->addInput(PluginsForm::HTitlebg());
+
+        // 打赏按钮跳动
+        $form->addInput(PluginsForm::ZanBump());
+
+        // 移动端禁止显示标签云和博客信息
+        $form->addInput(PluginsForm::MobileHideInfo());
+
+        // 首页文章鼠标经过上浮
+        $form->addInput(PluginsForm::IndexPostWave());
+
+        // 文章页头图悬浮
+        $form->addInput(PluginsForm::PostThumbImgWave());
+
+        // 网站运行时间
+        $form->addInput(PluginsForm::SiteSpendTime());
+
+        // 网站开始时间
+        $form->addInput(PluginsForm::SiteBegin());
+
+        // 文章end标识
+        $form->addInput(PluginsForm::PostEndMark());
+
+        // 文章二维码
+        $form->addInput(PluginsForm::PostQRCode());
+
+        // 百度手动提交按钮
+        $form->addInput(PluginsForm::BaiduPushBtn());
+
+        // 全站黑白模式
+        $form->addInput(PluginsForm::SiteBlackWhite());
+
+        // 文章版权提示
+        $form->addInput(PluginsForm::PostCopyrightTip());
+
+        // 评论边框
+        $form->addInput(PluginsForm::CommentBorder());
+
+        // 评论边框颜色RGB
+        $form->addInput(PluginsForm::CommentBorderRGB());
+
+        // 首页轮播图样式优化
+        $form->addInput(PluginsForm::IndexSwiperPicStyle());
+
+        // 首页图片版式优化
+        $form->addInput(PluginsForm::IndexHeadImageStyle());
+
+        // 设置静态背景图片开关
+        $form->addInput(PluginsForm::StaticBackgroundImage());
+
+        // 设置静态背景图片地址
+        $form->addInput(PluginsForm::StaticBackgroundImageUrl());
+
+        // 动态背景
+        $form->addInput(PluginsForm::DynamicBackground());
+
+        // 移动端隐藏动态背景
+        $form->addInput(PluginsForm::MobileHideDyBackground());
+
+        // 夜间模式隐藏动态背景
+        $form->addInput(PluginsForm::ThemeDarkHideDyBackground());
+
+        // 评论头像呼吸效果
+        $form->addInput(PluginsForm::CommentAvatarBreath());
+
+        // 文章前广告位
+        $form->addInput(PluginsForm::BeforePostContentAD());
+
+        // 评论前广告位
+        $form->addInput(PluginsForm::BeforeCommentListAD());
+    }
+
+    /**
+     * 个人用户的配置面板
+     *
+     * @param Form $form
+     */
+    public static function personalConfig(Form $form){}
+
+    /**
+     * 插件实现方法
+     *
+     * @access public
+     * @return void
+     */
+    public static function render(){}
+
+    /**
+     *为header添加css文件
+     *@access public
+     *@return void
+     */
+    public static function header() {
+
+        // 初始化头部
+        PluginsHead::init();
+
+        // iframe视频样式
+        PluginsHead::VideoIframe();
+
+        // 配置头部
+        PluginsHead::SettingHead();
+    }
+
+    /**
+     *为footer添加js文件
+     *@access public
+     *@return void
+     */
+    public static function footer() {
+
+        PluginsFooter::SettingFooter();
+
+    }
+
+
+    /**
+     *为editor_header添加js文件
+     *@access public
+     *@return void
+    **/
+    
+    public static function insertHeader()
+    {
+        echo "<script src='https://gcore.jsdelivr.net/gh/misaka10843/handsome-cdn@main/PrettyHandsome/static/js/extend.js'></script>";
+        echo "<script src='https://gcore.jsdelivr.net/gh/misaka10843/handsome-cdn@main/PrettyHandsome/static/js/edit.js'></script>";
+    }
+
+    /**
+     *短代码解析
+     *@access public
+     *@return void
+    **/
+    public static function parseShortCode(){
+		require_once 'ShortCode.php';
+		ShortCode::set('videoiframe',function($name,$attr,$text,$code){
+            $host = explode('.',$text, -1)[1];
+
+            switch ($host){
+                case "douyin":
+                    return '<iframe title="iframe" src="'. Helper::options()->plugin('PrettyHandsome')->dyapi . $text . '" frameborder="no"  framespacing="0" border="0" scrolling="no" allowfullscreen="true" class="iframe_video"></iframe>';
+                case "bilibili":
+                    return '<iframe title="iframe" src="' . $text . '&as_wide=1&high_quality=1&danmaku=0" frameborder="no" framespacing="0" border="0" scrolling="no" allowfullscreen="true" class="iframe_video"></iframe>';
+                default:
+                    return '<iframe title="iframe" src="' . Helper::options()->plugin('PrettyHandsome')->videoapi . $text . '" frameborder="no"  framespacing="0" border="0" scrolling="no" allowfullscreen="true" class="iframe_video"></iframe>';
+            }
+		});
+	}
+
+}
---
+++ PrettyHandsome/function/footer.php
@@ -0,0 +1,91 @@
+<?php
+	
+/*
+ * 作者：isSuperman
+ * 地址：https://github.com/isSuperman/Pretty_for_handsome
+ */
+ 
+class PluginsFooter{
+
+    // 配置插件
+    static function SettingFooter(){
+
+        // 评论前广告位
+        $beforeCommentListAD = Helper::options()->plugin('PrettyHandsome')->beforeCommentListAD;
+        echo '<script>if($("#post-comment-list").length){$("#post-comment-list").prepend("'.$beforeCommentListAD.'")}</script>';
+
+        // 文章前广告位
+        $beforePostAD = Helper::options()->plugin('PrettyHandsome')->beforePostAD;
+        echo '<script>let winPath=window.location.pathname;let pagePath=winPath.split("/")[1];if(pagePath!="image"){if($("#post-content").length){$("#post-content").prepend("'.$beforePostAD.'")}}</script>';
+        
+        // 文章底部版权提示
+        if(Helper::options()->plugin('PrettyHandsome')->postCopyrightTip==1){
+            echo '<script>if($("#post-content > div.support-author").length){addCopyrightTip();};</script>';
+        }
+
+        // 百度推送按钮
+        if(Helper::options()->plugin('PrettyHandsome')->baiduPush==1){
+            echo '<script>if($("#post-content > div.show-foot > div.notebook").length){addPushToBaiduBtn();};</script>';
+        }
+
+        // 文章二维码
+        if(Helper::options()->plugin('PrettyHandsome')->postQRcode==1){
+            echo '<script>addQRCodeIcon();if($("#qrcode").length){generateQRCode();};</script>';
+        }
+
+        // 文章end标识
+        if(Helper::options()->plugin('PrettyHandsome')->postQRcopostEndMarkde==1){
+            echo '<script>addEndMark();</script>';
+        }
+
+        // 访客数量和响应耗时
+        if(Helper::options()->plugin('PrettyHandsome')->siteInfo==1){
+            echo '<script>if(!$("#fkzs").length && !$("#xyhs").length){TotalVisit();ResponseTime();}</script>';
+        }
+
+        // 网站运行时间
+        if(Helper::options()->plugin('PrettyHandsome')->siteSpendTime==1){
+            echo '<script>window.setInterval(function(){var times=new Date().getTime()-Date.parse("'.Helper::options()->plugin('PrettyHandsome')->siteBegin.'");times=Math.floor(times/1000);var days=Math.floor(times/(60*60*24));times%=60*60*24;var hours=Math.floor(times/(60*60));times%=60*60;var minutes=Math.floor(times/60);times%=60;var seconds=Math.floor(times/1);$("#uptime").html(days+" 天 "+hours+" 时 "+minutes+" 分 "+seconds+" 秒 ")},1000);</script>';
+        }
+
+        // 时光流逝
+        if(Helper::options()->plugin('PrettyHandsome')->timeinfo==1){
+            echo '<script>if(window.windowWidth>=764){if(!$("#time_info").length){AddTimeInfo();}}</script>';
+        }
+
+        // 天气
+        if(Helper::options()->plugin('PrettyHandsome')->weather==1){
+            $weatherUID = Helper::options()->plugin('PrettyHandsome')->weatherUID;
+            $weatherHash = Helper::options()->plugin('PrettyHandsome')->weatherHash;
+            echo '<script>$("#header_right > ul").prepend(\'<div id="tp-weather-widget" class="navbar-form navbar-form-sm navbar-left shift"></div>\');(function(T,h,i,n,k,P,a,g,e){g=function(){P=h.createElement(i);a=h.getElementsByTagName(i)[0];P.src=k;P.charset="utf-8";P.async=1;a.parentNode.insertBefore(P,a)};T["ThinkPageWeatherWidgetObject"]=n;T[n]||(T[n]=function(){(T[n].q=T[n].q||[]).push(arguments)});T[n].l=+new Date();if(T.attachEvent){T.attachEvent("onload",g)}else{T.addEventListener("load",g,false)}}(window,document,"script","tpwidget","//widget.seniverse.com/widget/chameleon.js"));tpwidget("init",{"flavor":"slim","location":"WX4FBXXFKE4F","geolocation":"enabled","language":"auto","unit":"c","theme":"chameleon","container":"tp-weather-widget","bubble":"enabled","alarmType":"badge","color":"#C6C6C6","uid":"'.$weatherUID.'","hash":"'.$weatherHash.'"});tpwidget("show");</script>';
+        }
+
+        // 彩色目录
+         if(Helper::options()->plugin('PrettyHandsome')->colorToc==1){
+            echo '<script>colorToc();</script>';
+        }
+
+        // 彩色标签云
+        if(Helper::options()->plugin('PrettyHandsome')->colorTag==1){
+            echo '<script>colorTag();</script>';
+        }
+
+        // 鼠标点击特效
+        if(Helper::options()->plugin('PrettyHandsome')->clickWord==1){
+            echo '<script>var a_idx=0;jQuery(document).ready(function($){$("body").click(function(e){var a=new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");var $i=$("<span/>").text(a[a_idx]);a_idx=(a_idx+1)%a.length;var x=e.pageX,y=e.pageY;$i.css({"z-index":1e+69,"top":y-20,"left":x,"position":"absolute","font-weight":"bold","color":"#ff6651"});$("body").append($i);$i.animate({"top":y-180,"opacity":0},1500,function(){$i.remove()})})});</script>';
+        }
+
+        // 复制提示
+        if(Helper::options()->plugin('PrettyHandsome')->copyTip==1){
+            echo '<script>kaygb_copy();function kaygb_copy(){$(document).ready(function(){$("body").bind("copy",function(e){hellolayer()})});var sitesurl=window.location.href;function hellolayer(){$.message({message: "尊重原创，转载请注明出处！<br>"+sitesurl,title: "复制成功",type: "success",autoHide: !1,time: "5000"})}}</script>';
+        }
+
+        // 动态背景
+        $PluginPath = 'https://gcore.jsdelivr.net/gh/misaka10843/handsome-cdn@main/PrettyHandsome/static/';
+        
+        // 获取配置信息
+        $dynamicBackgroundtype = Helper::options()->plugin('PrettyHandsome')->dynamicBackground;
+        
+        echo '<script type="text/javascript" src="' . $PluginPath . 'js/bg/' . $dynamicBackgroundtype . '"></script>';
+    }
+}
